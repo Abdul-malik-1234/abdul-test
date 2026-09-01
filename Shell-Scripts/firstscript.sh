ace589b4b390b907ced9d94200e322048419d816
root <root@ip-172-31-209-169.ap-south-2.compute.internal>
2026-09-01 12:05:56 +0000

    feat(shell-script): Create a test shell script to test it using Jenkins

---
+++ Shell-Scripts/firstscript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#############################
+# Author: Abdul Malik Pasha #
+# Date:01-09-2026           #
+#############################
+echo "Hi i am executing from script"
+echo "*** Finished execution of scripts ****"
+
